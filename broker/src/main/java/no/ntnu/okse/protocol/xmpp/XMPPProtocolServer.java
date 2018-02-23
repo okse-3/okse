ee5cc8a4c09861f0130ed79442be7a31c52048c6
Philip <philippuente@hotmail.com>
2018-02-23 12:39:28 +0100

    Added class outlines

---
+++ broker/src/main/java/no/ntnu/okse/protocol/xmpp/XMPPProtocolServer.java
@@ -0,0 +1,32 @@
+package no.ntnu.okse.protocol.xmpp;
+
+import no.ntnu.okse.core.messaging.Message;
+import no.ntnu.okse.protocol.AbstractProtocolServer;
+
+public class XMPPProtocolServer extends AbstractProtocolServer {
+
+  @Override
+  public void boot() {
+
+  }
+
+  @Override
+  public void run() {
+
+  }
+
+  @Override
+  public void stopServer() {
+
+  }
+
+  @Override
+  public String getProtocolServerType() {
+    return null;
+  }
+
+  @Override
+  public void sendMessage(Message message) {
+
+  }
+}
---
+++ broker/src/main/java/no/ntnu/okse/protocol/xmpp/XMPPServer.java
@@ -0,0 +1,9 @@
+package no.ntnu.okse.protocol.xmpp;
+
+import no.ntnu.okse.web.Server;
+
+public class XMPPServer extends Server{
+
+  //send and receive methods are to be implemented here
+
+}
