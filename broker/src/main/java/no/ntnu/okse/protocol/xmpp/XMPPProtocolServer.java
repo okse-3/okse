096abddd1fad5de0035a69eff400fbe931230c17
Philip <philippuente@hotmail.com>
2018-02-23 15:46:18 +0100

    Added base XMPPProtocolServer methods and method signatures in other classes

--- broker/src/main/java/no/ntnu/okse/protocol/xmpp/XMPPProtocolServer.java
+++ broker/src/main/java/no/ntnu/okse/protocol/xmpp/XMPPProtocolServer.java
@@ -2,12 +2,37 @@ package no.ntnu.okse.protocol.xmpp;
 
 import no.ntnu.okse.core.messaging.Message;
 import no.ntnu.okse.protocol.AbstractProtocolServer;
+import org.apache.log4j.Logger;
 
 public class XMPPProtocolServer extends AbstractProtocolServer {
 
+  protected static final String SERVERTYPE = "xmpp";
+
+  private XMPPServer server;
+
+  private static Logger log = Logger.getLogger(XMPPProtocolServer.class.getName());
+
+  /**
+   * Constructor for the XMPP protocol server
+   * @param host, host ip as a {@link String}
+   * @param port, port number as a {@link Integer}
+   */
+  public XMPPProtocolServer(String host, Integer port) {
+    this.port = port;
+    this.host = host;
+    this.protocolServerType = SERVERTYPE;
+  }
+
   @Override
   public void boot() {
-
+    if (!_running) {
+      server = new XMPPServer(host, port);
+      _serverThread = new Thread(this::run);
+      _serverThread.setName("XMPPProtocolServer");
+      _serverThread.start();
+      _running = true;
+      log.info("XMPPProtocolServer booted successfully");
+    }
   }
 
   @Override
@@ -17,16 +42,22 @@ public class XMPPProtocolServer extends AbstractProtocolServer {
 
   @Override
   public void stopServer() {
+    log.info("Stopping XMPPProtocolServer");
+    server.stopServer();
 
   }
 
   @Override
   public String getProtocolServerType() {
-    return null;
+    return SERVERTYPE;
   }
 
   @Override
   public void sendMessage(Message message) {
+    server.sendMessage(message);
+  }
 
+  public boolean isRunning() {
+    return _running;
   }
 }
--- broker/src/main/java/no/ntnu/okse/protocol/xmpp/XMPPServer.java
+++ broker/src/main/java/no/ntnu/okse/protocol/xmpp/XMPPServer.java
@@ -1,8 +1,21 @@
 package no.ntnu.okse.protocol.xmpp;
 
+import no.ntnu.okse.core.messaging.Message;
 import no.ntnu.okse.web.Server;
 
 public class XMPPServer extends Server{
+
+
+
+  public XMPPServer(String host, Integer port) {
+    super();
+  }
+
+  public void stopServer() {
+  }
+
+  public void sendMessage(Message message) {
+  }
 
   //send and receive methods are to be implemented here
 
