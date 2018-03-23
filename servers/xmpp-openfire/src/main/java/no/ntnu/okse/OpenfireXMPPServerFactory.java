d0084fd98850277eff5a8e9d304db70dd62a66ee
zootos <halvard.hummel@hotmail.com>
2018-03-23 10:48:17 +0100

    Check if the domain is correct between the database and config

--- servers/xmpp-openfire/src/main/java/no/ntnu/okse/OpenfireXMPPServerFactory.java
+++ servers/xmpp-openfire/src/main/java/no/ntnu/okse/OpenfireXMPPServerFactory.java
@@ -2,11 +2,15 @@ package no.ntnu.okse;
 
 import java.io.IOException;
 import org.jivesoftware.openfire.XMPPServer;
+import org.jivesoftware.util.JiveGlobals;
 
 public class OpenfireXMPPServerFactory {
 
   private static XMPPServer xmppServer;
 
+  /**
+   * Starts the Openfire XMPP server
+   */
   public static void startXMPPServer() {
     if (serverRunning()) {
       throw new IllegalStateException("XMPP server already running");
@@ -20,12 +24,35 @@ public class OpenfireXMPPServerFactory {
 
     xmppServer = new XMPPServer();
     xmppServer.finishSetup();
-    // Listen for clients on
-    xmppServer.getConnectionManager().enableClientListener(true);
-    // Everyone should be able to create nodes
-    xmppServer.getPubSubModule().setNodeCreationRestricted(true);
+
+    // If the domain is not the same in the database and in the config, reset database
+    if (!isCorrectDomain()) {
+      OpenfireXMPPServerDBManager.deleteDatabase();
+      startXMPPServer();
+    } else {
+      // Listen for clients on
+      xmppServer.getConnectionManager().enableClientListener(true);
+      // Everyone should be able to create nodes
+      xmppServer.getPubSubModule().setNodeCreationRestricted(true);
+    }
   }
 
+  /**
+   * Checks if the domain in the database is the same as the one in the config file
+   *
+   * @return A boolean indicating if the domain is the same
+   */
+  private static boolean isCorrectDomain() {
+    if (!serverRunning()) {
+      throw new IllegalStateException("XMPP server is not running");
+    }
+    String domain = xmppServer.getPubSubModule().getAddress().getDomain();
+    return JiveGlobals.getProperty("xmpp.domain").equals(domain.substring(7));
+  }
+
+  /**
+   * Stops the Openfire XMPP server
+   */
   public static void stopXMPPServer() {
     if (!serverRunning()) {
       throw new IllegalStateException("XMPP server is already stopped");
@@ -33,6 +60,11 @@ public class OpenfireXMPPServerFactory {
     xmppServer.stop();
   }
 
+  /**
+   * Checks if the Openfire XMPP server is running
+   *
+   * @return A boolean indicating if the XMPP server is running
+   */
   public static boolean serverRunning() {
     return xmppServer != null && xmppServer.isStarted() && !xmppServer.isShuttingDown();
   }
