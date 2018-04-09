afd577fe33f8d4082c52916f49bbd87799ad0b64
zootos <halvard.hummel@hotmail.com>
2018-04-09 16:29:02 +0200

    Implement MQTT SN Client

--- clients/common/src/main/java/no/ntnu/okse/clients/mqttsn/MQTTSNClient.java
+++ clients/common/src/main/java/no/ntnu/okse/clients/mqttsn/MQTTSNClient.java
@@ -1,31 +1,22 @@
 package no.ntnu.okse.clients.mqttsn;
 
 import no.ntnu.okse.clients.TestClient;
+import org.eclipse.paho.mqttsn.udpclient.SimpleMqttsClient;
 
-public class MQTTSNClient implements TestClient {
+/**
+ * By extending SimpleMqttsClient we gain almost everything for free
+ */
+public class MQTTSNClient extends SimpleMqttsClient implements TestClient {
 
-  @Override
-  public void connect() {
-
+  public MQTTSNClient() {
+    super("localhost", 20000);
   }
 
-  @Override
-  public void disconnect() {
-
-  }
-
-  @Override
-  public void subscribe(String topic) {
-
-  }
-
-  @Override
-  public void unsubscribe(String topic) {
-
-  }
-
+  /**
+   * Need to convert content to bytes to be able to use the base class
+   */
   @Override
   public void publish(String topic, String content) {
-    
+    publish(topic, content.getBytes());
   }
 }
