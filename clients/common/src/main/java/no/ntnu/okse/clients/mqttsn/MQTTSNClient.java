ef22947d86c5b0a408fd258d540377adb462f8e1
zootos <halvard.hummel@hotmail.com>
2018-04-09 13:50:00 +0200

    Outline for client class

---
+++ clients/common/src/main/java/no/ntnu/okse/clients/mqttsn/MQTTSNClient.java
@@ -0,0 +1,31 @@
+package no.ntnu.okse.clients.mqttsn;
+
+import no.ntnu.okse.clients.TestClient;
+
+public class MQTTSNClient implements TestClient {
+
+  @Override
+  public void connect() {
+
+  }
+
+  @Override
+  public void disconnect() {
+
+  }
+
+  @Override
+  public void subscribe(String topic) {
+
+  }
+
+  @Override
+  public void unsubscribe(String topic) {
+
+  }
+
+  @Override
+  public void publish(String topic, String content) {
+    
+  }
+}
