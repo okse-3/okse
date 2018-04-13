4f72596d85bceaef35b19ee64725ce82997a4432
Adrian Junger Steffenakk <adrian.steffenakk@gmail.com>
2018-04-13 14:49:28 +0200

    Update to tests. 3 of 5 now working. Create/send message and subscribe/unsubscribe still not working.

--- broker/src/test/java/no/ntnu/okse/protocol/xmpp/XMPPServerTest.java
+++ broker/src/test/java/no/ntnu/okse/protocol/xmpp/XMPPServerTest.java
@@ -5,12 +5,17 @@ import static org.testng.Assert.*;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import no.ntnu.okse.OpenfireXMPPServerFactory;
 import no.ntnu.okse.clients.xmpp.XMPPClient;
 import no.ntnu.okse.core.Utilities;
 import no.ntnu.okse.core.messaging.Message;
+import org.jivesoftware.smackx.pubsub.Item;
 import org.jivesoftware.smackx.pubsub.LeafNode;
+import org.jivesoftware.smackx.pubsub.Node;
+import org.jivesoftware.smackx.pubsub.PayloadItem;
 import org.jxmpp.jid.impl.JidCreate;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -27,8 +32,7 @@ public class XMPPServerTest {
   //@Mock(name = "server")
   private XMPPServer server;
 
-  private XMPPClient client;
-
+  private ArrayList<Item> itemList = new ArrayList<>();
 
 
   @BeforeClass
@@ -38,58 +42,59 @@ public class XMPPServerTest {
     OpenfireXMPPServerFactory.start();
     // Make sure the server starts
     Thread.sleep(5000);
-    XMPPProtocolServerUtil.start();
+    ps = new XMPPProtocolServer("localhost", 5222, "okse@localhost", "pass");
+    ps.boot();
     Thread.sleep(1000);
-    client = new XMPPClient("testClient", "localhost", 5222);
-    client.connect();
+    server = ps.getServer();
   }
 
   @AfterClass
   private void tearDown() throws Exception {
-    client.disconnect();
-    client = null;
     ps.stopServer();
+    server = null;
     OpenfireXMPPServerFactory.stop();
   }
 
   @Test
   public void bootServerTest() {
     assertTrue(OpenfireXMPPServerFactory.isRunning());
-    assertTrue(XMPPProtocolServerUtil.isRunning());
-    assertNotNull(this.client);
-//    assertNotNull(this.server);
+    assertTrue(ps.isRunning());
+    assertNotNull(server);
   }
 
   @Test
   public void testCreateOrGetLeafNode() throws Exception {
-    //assertNotNull(server.getLeafNode("testTopic"));
+    assertNotNull(server.getLeafNode("testTopic"));
   }
+
 
   @Test
   public void testCreateAndSendMessage() throws Exception {
-    client.subscribe("testTopic");
-    int oldCount = client.messageCounter;
-    //server.sendMessage(new Message("testMessage", "testTopic", null, ps.getProtocolServerType()));
-    client.publish("testTopic", "testMessage");
-    assertTrue(client.messageCounter == oldCount + 1);
+    LeafNode nodeToCheck = server.getLeafNode("testTopic");
+    assertEquals(nodeToCheck.getItems().size(), 0);
+    server.sendMessage(new Message("testMessage", "testTopic", null, ps.getProtocolServerType()));
+    assertEquals(nodeToCheck.getItems().size(), 1);
   }
 
   @Test
   public void testSubscribeUnsubscribeToNode() throws Exception {
-    ConcurrentHashMap listenerMap = (ConcurrentHashMap) server.getClass().getField("listenerMap").get(server);
+    Node nodeToCheck = server.getLeafNode("testTopic");
+    assertEquals(nodeToCheck.getSubscriptions().size(), 1);
     server.subscribeToTopic("testTopic");
-    assertTrue(listenerMap.get("testTopic") != null);
+    assertEquals(nodeToCheck.getSubscriptions().size(), 2);
+    server.unsubscribeFromTopic("testTopic");
+    assertEquals(nodeToCheck.getSubscriptions().size(), 1);
   }
 
   @Test
   public void testOnMessageReceived() throws Exception {
+    itemList.add(new PayloadItem<>("item1", new Item()));
     int messageCount = ps.getTotalMessagesReceived();
-    server.subscribeToTopic("testTopic");
-    client.publish("testTopic", "testMessage");
-    assertEquals(messageCount + 1, ps.getTotalMessagesReceived());
+    server.onMessageReceived(itemList, "testTopic");
+    assertEquals(ps.getTotalMessagesReceived(), messageCount + 1);
   }
 
-  @Test
+
 
 
   //Following test is not possible based on current architecture. Please conduct XMPP to XMPP communication tests manually.
