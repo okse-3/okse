47f4acff88131ca8fc8de830630d186a0c83bdfb
zootos <halvard.hummel@hotmail.com>
2018-05-05 17:29:44 +0200

    Fixes to shutdown of subscribe client

--- clients/common/src/main/java/no/ntnu/okse/clients/SubscribeClient.java
+++ clients/common/src/main/java/no/ntnu/okse/clients/SubscribeClient.java
@@ -1,25 +1,29 @@
 package no.ntnu.okse.clients;
 
 import com.beust.jcommander.Parameter;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class SubscribeClient extends CommandClient {
 
   @Parameter(names = {"-n"}, description = "Number of messages to receive")
   public int numberOfMessages = Integer.MAX_VALUE;
 
-  private int numberOfReceivedMessages = 0;
+  private AtomicInteger numberOfReceivedMessages = new AtomicInteger(0);
+  private Thread shutdownHook;
+  private TestClient client;
 
   public void run() {
     initLogger();
     createClient();
-    TestClient client = getClient();
+    client = getClient();
     client.connect();
     topics.forEach(this::subscribe);
     // Graceful disconnect
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      topics.forEach(client::unsubscribe);
-      client.disconnect();
-    }));
+    shutdownHook = new Thread(() -> shutdown(true));
+    Runtime.getRuntime().addShutdownHook(shutdownHook);
     System.out.println("Listening for messages...");
     try {
       Thread.sleep(10000000);
@@ -33,13 +37,34 @@ public abstract class SubscribeClient extends CommandClient {
   }
 
   public void receiveMessage(String topic, String message, boolean shouldPrint) {
-    numberOfReceivedMessages++;
+    int messageNumber = numberOfReceivedMessages.incrementAndGet();
     if (shouldPrint) {
       System.out.println(String.format("Recieved message #%d on topic %s with content %s",
-          numberOfReceivedMessages, topic, message));
+          messageNumber, topic, message));
     }
-    if (numberOfMessages == numberOfReceivedMessages) {
+    if (numberOfMessages == messageNumber) {
       System.out.println("Received indicated number of messages, un-subscribing and disconnecting");
+      Runtime.getRuntime().removeShutdownHook(shutdownHook);
+      // In case the unsubscribe/disconnect is not working correctly
+      new Timer().schedule(new TimerTask() {
+        @Override
+        public void run() {
+          System.out
+              .println("Un-subscribing and disconnect is taking too long. Forcing a shutdown");
+          System.exit(1);
+        }
+      }, new Date(System.currentTimeMillis() + 10000));
+      new Thread(() -> shutdown(false)).start();
+    }
+  }
+
+  private void shutdown(boolean isHook) {
+    try {
+      topics.forEach(client::unsubscribe);
+    } catch (Exception ignored) {
+    }
+    client.disconnect();
+    if (!isHook) {
       System.exit(0);
     }
   }
